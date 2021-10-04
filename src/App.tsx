ba1a6c84469cd29408f765c7bec81016480db861
willy <shen.willy95@gmail.com>
2021-10-04 23:24:56 +0700

    feat: init ui

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,40 @@ import Main from "./pages/Main/Main";
 // import { CHAIN_ID } from "./constants"
 // import Redeem from "./pages/Redeem"
 // import Settle from "./pages/Settle"
+import Logo from "./assets/dango-placeholder.png";
 
 function App() {
   // const { chainId } = useEthereum();
   return (
     <Router>
-      <div>
-        {/* <Header /> */}
-        <div>
-          <Switch>
-            {/* <Route path="/redeem">
-              <Redeem />
-            </Route>
-            <Route path="/settle">
-              <Settle />
-            </Route> */}
-            <Route path="/">
-              <Main />
-            </Route>
-          </Switch>
+      <div className="flex-col h-screen">
+        <div className="fixed flex w-screen justify-between py-3 px-5">
+          <div className="flex items-center">
+            <img src={Logo} className="h-7"/>
+            <span className="text-2xl font-mono font-light mx-2">
+              Dango
+            </span>
+          </div>
+          <button className="bg-blue-400 hover:bg-blue-500 text-white px-8 py-2 rounded-xl font-mono">Connect</button>
         </div>
+        <div className="bg-red-100 h-full">
+          {/* <Header /> */}
+          <div>
+            <Switch>
+              {/* <Route path="/redeem">
+                <Redeem />
+              </Route>
+              <Route path="/settle">
+                <Settle />
+              </Route> */}
+              <Route path="/">
+                <Main />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+        {/* <ModalNetwork isOpen={chainId !== undefined && chainId !== CHAIN_ID.MAINNET} /> */}
       </div>
-      {/* <ModalNetwork isOpen={chainId !== undefined && chainId !== CHAIN_ID.MAINNET} /> */}
     </Router>
   );
 }
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,25 @@
 
 const Main = () => {
     return (
-        <>
-            Main
-        </>
+      <div className="w-full flex h-screen">
+          <div className="w-full sm:w-3/4 lg:w-2/3 m-auto">
+            <div className="grid grid-cols-3">
+              <div className="col-span-2 bg-white rounded-lg p-4 m-2">
+                <a className="font-bold" href="#">
+                  Learn about Dollar-Cost-Averaging using Dango ↗
+                </a>
+              </div>
+                <button className="bg-red-400 hover:bg-red-500 rounded-lg p-4 m-2 font-mono font-bold text-white">
+                  + New Position
+                </button>
+            </div>
+            <div className="bg-white rounded-lg p-4 m-2 h-52 flex flex-col items-center justify-center">
+                <span className="font-mono">
+                  Your DCA positions will appear here.
+                </span>
+            </div>
+          </div>
+      </div>  
     );
 };
 
