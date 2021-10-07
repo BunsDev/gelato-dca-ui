4b62774bc19b094156ebf101ee2ab27592e869ab
willy <shen.willy95@gmail.com>
2021-10-07 12:08:57 +0700

    feat: add assets

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,6 +1,7 @@
-
-import { BsArrowRightShort } from "react-icons/bs";
+import { BsArrowRightShort, BsChevronDown, BsQuestionCircle } from "react-icons/bs";
 import ButtonBack from "../../components/ButtonBack/ButtonBack";
+import UsdcLogo from "../../assets/usdc.png";
+import EthLogo from "../../assets/eth.png";
 
 const Create = () => {
     return (
@@ -12,28 +13,36 @@ const Create = () => {
                 Open DCA Position
               </div>
               <div className="flex mt-4 items-center">
-                <div>
-                  <div className="text-md font-bold">Token to Send</div>
-                  <div className="text-xl">USDC</div>
+                <div className="w-1/4">
+                  <div className="text-md">Token to Send</div>
+                  <div className="border border-gray-300 rounded-full hover:bg-gray-200 px-4 py-2 mt-1 cursor-pointer flex items-center">
+                    <img src={UsdcLogo} className="h-5 pr-2"/>
+                    <div className="text-lg">USDC</div>
+                    <BsChevronDown className="ml-auto inline" size="18px"/>
+                  </div>
                 </div>
                 <BsArrowRightShort className="mx-3" size="20px"/>
-                <div>
-                  <div className="text-md font-bold">Token to Receive</div>
-                  <div className="text-xl">ETH</div>
+                <div className="w-1/4">
+                  <div className="text-md">Token to Receive</div>
+                  <div className="border border-gray-300 rounded-full hover:bg-gray-200 px-4 py-2 mt-1 cursor-pointer flex items-center">
+                    <img src={EthLogo} className="h-5 pr-2"/>
+                    <div className="text-lg">ETH</div>
+                    <BsChevronDown className="ml-auto inline" size="18px"/>
+                  </div>
                 </div>
               </div>
-              <div className="grid grid-cols-3 gap-3 mt-3">
+              <div className="mt-5">
+                <div className="text-md">Deposit Funds<BsQuestionCircle className="inline pl-1 pb-1" size="18px"/></div>
+                <div className="text-xl mt-1">1,000 USDC</div>
+              </div>
+              <div className="grid grid-cols-3 gap-3 mt-5">
                 <div>
-                  <div className="text-md font-bold">Initial Funds</div>
-                  <div className="text-xl">1,000 USDC</div>
+                  <div className="text-md">DCA Amount<BsQuestionCircle className="inline pl-1 pb-1" size="18px"/></div>
+                  <div className="text-xl mt-1">100 USDC</div>
                 </div>
                 <div>
-                  <div className="text-md font-bold">DCA Amount</div>
-                  <div className="text-xl">100 USDC</div>
-                </div>
-                <div>
-                  <div className="text-md font-bold">DCA Interval</div>
-                  <div className="text-xl">1 Hour</div>
+                  <div className="text-md">DCA Interval<BsQuestionCircle className="inline pl-1 pb-1" size="18px"/></div>
+                  <div className="text-xl mt-1">1 Hour</div>
                 </div>
               </div>
               <div className="flex mt-8">
@@ -43,7 +52,7 @@ const Create = () => {
               </div>
             </div>
           </div>
-      </div>  
+      </div>
     );
 };
 
