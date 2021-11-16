9bbfae579bb81dda650f25baac137b23e73b5e2f
willy <shen.willy95@gmail.com>
2021-11-16 14:08:49 +0700

    feat: button isDisabled

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,20 +5,27 @@ interface ButtonProps {
   isPrimary?: boolean;
   isBold?: boolean;
   isMono?: boolean;
+  isDisabled?: boolean;
   fullWidth?: boolean;
   padding?: string; //unchecked
   fontSize?: string; //unchecked
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, isPrimary=true, isBold=false, isMono=true, fullWidth=true, padding, fontSize, onClick }) => {
-  
+const Button: React.FC<ButtonProps> = ({ 
+  label, isPrimary=true, isBold=false, isMono=true, isDisabled=false, fullWidth=true, padding, fontSize, onClick 
+}) => {
   const className = useMemo(() => {
     let classes = "rounded-lg text-white";
-    if (isPrimary) {
-      classes += " bg-red-400 hover:bg-red-500"
+
+    if (isDisabled) {
+      classes += " bg-gray-400"
     } else {
-      classes += " bg-blue-400 hover:bg-blue-500"
+      if (isPrimary) {
+        classes += " bg-red-400 hover:bg-red-500"
+      } else {
+        classes += " bg-blue-400 hover:bg-blue-500"
+      }
     }
 
     if (isBold) {
@@ -42,7 +49,7 @@ const Button: React.FC<ButtonProps> = ({ label, isPrimary=true, isBold=false, is
     classes += ` ${padding} ${fontSize}`;
 
     return classes;
-  }, [isPrimary, isBold, isMono, fontSize, fullWidth, padding]);
+  }, [isPrimary, isBold, isMono, isDisabled, fontSize, fullWidth, padding]);
   
   return (
     <button className={className} onClick={onClick}>
