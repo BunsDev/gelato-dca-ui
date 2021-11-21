f9fd1c8c714ebd50cf5a5fb3acafb99e8eab779d
willy <shen.willy95@gmail.com>
2021-11-21 21:49:36 +0700

    feat: update contract address

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -1,3 +1,3 @@
-export const DCA_CORE_ADDRESS = "0x52edfb74871903e659d945fa810f4b4f2a8e54fb";
+export const DCA_CORE_ADDRESS = "0x772b1b3fe659d633cdf673e74b342bf511dfdfcf";
 export const WMATIC_ADDRESS = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
 export const NATIVE_TOKEN = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
